9e6be0c47f4cc928b8909b9fe1a8e04163f5990f
Gfrey <gfreycode@gmail.com>
2023-04-02 16:06:54 -0500

    any

---
+++ src/07-array.ts
@@ -0,0 +1,33 @@
+(()=>{
+
+  const prices: number[] = [120,220,320,420,520,620,720];
+  const strings: string[] = ['one', 'two', 'three', 'four'];
+
+  const strAndNumbers = [1, 'two', 3, 'four'];
+  const strAndNumbers2: (string | number)[] = [1, 'two', 3, 'four'];
+
+  const mixed = prices.map(price => {
+    if(price > 400) {
+      return String(price)
+    }
+    if(price > 200 && price < 400) {
+      return price
+    } else {
+      return price === price;
+    }
+  });
+  console.log(mixed);
+
+  const arr: (string|number)[] = [];
+  arr.push('Hello');
+  arr.push(101145);
+  arr.push(0b1011000);
+  arr.push(0xf54f49);
+  console.log(arr);
+  const prices2: number[] = [120,220,320,420,520,620,720];
+  const sum = prices2.map((num, index) => num * index);
+
+  console.log(sum);
+
+
+})()
