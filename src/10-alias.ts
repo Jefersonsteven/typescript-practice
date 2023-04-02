6290845b55b17592084968598b1a0ae72cb73215
Gfrey <gfreycode@gmail.com>
2023-04-02 16:56:13 -0500

    null y undefined

---
+++ src/10-alias.ts
@@ -0,0 +1,19 @@
+(()=>{
+
+  type UserID =  string | number | boolean;
+  let userId: UserID;
+
+  function greeting(userId: UserID) {
+    if(typeof userId === 'string') {
+      return userId.split('').map(c => c + '*').join('');
+    }
+  }
+
+  // literal types
+  type SizeShirt = 'S' | 'M' | 'L' | 'XL';
+  let shirtSize: SizeShirt;
+  shirtSize = 'M';
+  shirtSize = 'L';
+  shirtSize = 'XL';
+  // shirtSize = 'sdawdas'; //! error
+})()
